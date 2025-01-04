2eebc94c3f947aff88c5038cc471e4b48854ee41
Hari-Hrk <hrk.harish87@gmail.com>
2025-01-04 13:41:06 +0530

    fastapi with model done

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -18,6 +18,7 @@ from sensor.constant.training_pipeline import SAVED_MODEL_DIR
 
 
 class TrainPipeline:
+    is_pipeline_running = False
 
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
@@ -138,6 +139,8 @@ class TrainPipeline:
 
     def run_pipeline(self):
         try:
+
+            TrainPipeline.is_pipeline_running = True
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
 
 
@@ -156,9 +159,12 @@ class TrainPipeline:
             if not model_eval_artifact.is_model_accepted:
                 raise Exception("Trained model is not better than the best model")  
 
-            model_eval_artifact = self.start_model_pusher(model_eval_artifact) 
+            model_pusher_artifact = self.start_model_pusher(model_eval_artifact)       
 
-        except Exception as e :    
+            TrainPipeline.is_pipeline_running = False
+        except Exception as e : 
+            TrainPipeline.is_pipeline_running = False
+ 
             raise  SensorException(e,sys)
 
 
